a199121ff7f5eb181652e9070a1209ccce7dfa34
Yunus Oksuz <yunusoksuz@gmail.com>
2017-12-04 23:46:13 +0300

    adana env #2

--- src/env/adana.js
+++ src/env/adana.js
@@ -1,7 +1,8 @@
 (function () {
     var appConfig = {
       api: 'http://sistem.carlsjradana.com/api',
-      mapsApiKey: 'AIzaSyCzJj2IW8r5jbzxRUT3k0pE_h1AaQsSm50'
+      mapsApiKey: 'AIzaSyCzJj2IW8r5jbzxRUT3k0pE_h1AaQsSm50',
+      publicLocationUrl: 'https://takip.carlsjradana.com/#hash#',
     };
 
     angular.module('OpenMtsWebCli').constant('appConfig', appConfig);
